2dbdc008612be816aeea14f5811ddf593cf5bde3
glisowski <lisux1991@tlen.pl>
2014-04-26 12:59:51 +0200

    movement complete

--- Script/ShipMovement.cs
+++ Script/ShipMovement.cs
@@ -5,6 +5,7 @@ public class ShipMovement : MonoBehaviour {
 
 
 	public float force = 10;
+	public float maxVelocity = 20;
 
 	// Use this for initialization
 	void Start () {
@@ -14,9 +15,13 @@ public class ShipMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(transform && rigidbody){
-			rigidbody.AddForce(transform.forward * Input.GetAxis("Throttle") * force);
+			rigidbody.AddForce(transform.forward * -Input.GetAxis("Throttle") * force);
 			rigidbody.AddForce(transform.up * Input.GetAxis("Vertical") * force);
 			rigidbody.AddForce(transform.right * Input.GetAxis("Horizontal") * force);
+			if(rigidbody.velocity.sqrMagnitude > maxVelocity)
+			{
+				rigidbody.velocity *= 0.99f;
+			}
 		}
 	}
 }
